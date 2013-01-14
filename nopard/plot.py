5eb54fc3015a2745625e4b1306c4346ab66c3099
Ingo Fründ <ingo.fruend@googlemail.com>
2013-01-14 16:42:13 -0500

    Adding qqplot -- not always working though

--- nopard/plot.py
+++ nopard/plot.py
@@ -20,5 +20,31 @@ def dotplot ( x, y=0 ):
     n = len(x)
     pl.plot ( x, (.2*pl.log10(n)/S * pl.randn(len(x)))+y, '.', color=[.7]*3 )
 
-# Just copy the show function then we don't need to import pylab everytime
+def qqplot ( th, D, interval=(0,1) ):
+    """Create a q-q plot of theta with respect to density D
+
+    :Parameters:
+        *th*
+            a sample that might be from the density D
+        *D*
+            a density object
+    """
+    th = th.copy()
+    th.sort()
+    nq = len(th)
+    q = (pl.arange ( 0, nq, dtype='d' )+1)/nq
+    x = pl.mgrid[interval[0]:interval[1]:1j*4*nq]
+    f = D(x)
+    qD = pl.cumsum(f)*pl.diff(x)[0]/pl.trapz(f,x)
+    th_ = []
+    for q_ in q:
+        i = pl.where ( qD<=q_ )[0]
+        th_.append ( x[i[-1]] )
+
+    pl.plot ( th, th_, '.' )
+    pl.plot ( [th[0],th[-1]],[th[0],th[-1]], 'k:' )
+    # pl.plot ( th, q )
+    # pl.plot ( x, qD )
+
+# Just copy the show function so that we don't need to import pylab everytime
 show = pl.show
