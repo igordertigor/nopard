5103f393f2d2216cef70b9f628d6bd8cb613bca9
Ingo Fründ <ingo.fruend@googlemail.com>
2013-03-15 18:27:41 -0400

    Including 2d density estimation

--- nopard/orthogonal.py
+++ nopard/orthogonal.py
@@ -6,6 +6,7 @@ in Wasserman: All of statistics.
 
 import numpy as np
 from scipy import stats
+from scipy import special
 
 twopi = (2*np.pi)
 
@@ -13,6 +14,16 @@ class orthogonalBasis ( object ):
     """Base class for orthogonal bases"""
     def __call__ ( self, j, x ):
         pass
+
+class LegendreBasis ( orthogonalBasis ):
+    """basis of legendre polynomials"""
+    def __init__ ( self ):
+        self.K = 2
+    def __call__ ( self, j, x ):
+        if j>40:
+            print "WARNING: legendre polynomials for j>40 are numerically unstable"
+        P = np.poly1d ( special.legendre ( j ) )
+        return P(2*x-1)
 
 class CosineBasis ( orthogonalBasis ):
     """Cosine basis Phi_j = sqrt(2)*cos(pi*j*x)"""
@@ -77,9 +88,12 @@ class orthogonalDensity ( object ):
             self.sg = np.zeros ( nmax, 'd' )
             self.nmax = nmax
             self.R = np.zeros ( nmax, 'd' )
-            self.__fit( self.scale*data )
+            self.__fit( self.scale*(data-self.x0) )
 
-        self.__normalize ()
+        if not isinstance ( basis, orthogonal2d ):
+            self.__normalize1d ()
+        else:
+            self.__normalize2d ()
 
     def __fit ( self, data ):
         n = len(data)
@@ -99,22 +113,50 @@ class orthogonalDensity ( object ):
         self.c = self.Phi.K**2 * \
                 np.sqrt ( self.jmax*stats.chisqprob ( self.alpha, self.jmax )/n )
 
-    def __normalize ( self ):
+    def __normalize1d ( self ):
         # Perform numerical integration to normalize the nonnegative density
         x = np.mgrid[self.x0:self.interval_length:100j]
         f = self(x)
         self.Z = np.trapz ( f, x )
+    def __normalize2d ( self ):
+        x,y = np.mgrid[self.x0:self.interval_length:100j,self.x0:self.interval_length:100j]
+        f = self(np.c_[x.ravel(),y.ravel()]).reshape(x.shape)
+        self.Z = np.trapz ( np.trapz (f,x[:,0],axis=0),y[0,:])
 
     def __call__ ( self, x ):
         """Evaluates the density at position x"""
-        out = 0.*x
+        out = np.zeros ( x.shape[0], 'd' )
         for j in xrange ( self.jmax ):
-            out += self.b[j]*self.Phi(j, self.scale*x )
+            out += self.b[j]*self.Phi(j, self.scale*(x-self.x0) )
         return np.clip ( out/self.scale/self.Z, 0, 1e10 )
 
     def __str__ ( self ):
         return "<orthogonalDensity on (%g,%g), basis: %s, J^=%g>" % \
                 (self.x0,self.x0+self.interval_length,str(self.Phi),self.jmax)
+
+# 2d
+
+class orthogonal2d ( orthogonalBasis ):
+    def __init__ ( self, Basis1d ):
+        self.basis1d = Basis1d
+        self.K = self.basis1d.K
+    def __call__ ( self, j, x ):
+        k,l = self.__map_j_to_kl ( j )
+        return self.basis1d ( k,x[:,0] )*self.basis1d ( l,x[:,1] )
+    def __map_j_to_kl ( self, j ):
+        k = l = 0
+        d = 1
+        count = 0
+        while count < j:
+            for k in xrange ( d+1 ):
+                l = d-k
+                count += 1
+                if count == j:
+                    break
+            d += 1
+        return k,l
+    def __str__ ( self ):
+        return "ortogonal2d (%s)" % (str(self.basis1d))
 
 if __name__ == "__main__":
     import plot
